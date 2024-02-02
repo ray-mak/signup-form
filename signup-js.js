261708f8741b78e748453579d0db6d2edb9c8c30
Ray Mak <projectraymak@gmail.com>
2024-02-02 12:37:39 -0800

    Second Commit - edited appearance and shortened JS

--- signup-js.js
+++ signup-js.js
@@ -3,14 +3,6 @@ const lastName = document.getElementById("last-name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const claimBtn = document.getElementById("claim-btn");
-const fnIcon = document.getElementById("fn-icon");
-const fnNotice = document.getElementById("fn-notice");
-const lnIcon = document.getElementById("ln-icon");
-const lnNotice = document.getElementById("ln-notice");
-const emailIcon = document.getElementById("email-icon");
-const emailNotice = document.getElementById("email-notice");
-const pwIcon = document.getElementById("pw-icon");
-const pwNotice = document.getElementById("pw-notice");
 
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
@@ -21,56 +13,16 @@ const validateEmail = () =>{
     return false;
 }
 
-const checkInputs = (e) => {
-    checkFn();
-    checkLn();
-    checkEmail();
-    checkPw();
-    return
-}
-
-const checkFn = () => {
-    if (firstName.value == "") {
-        fnIcon.classList.remove("hidden");
-        fnNotice.innerHTML = `
-        <p class="notification pink"><em>First Name cannot be empty</em></p>`
-    } else if (firstName.value != "") {
-        fnIcon.classList.add("hidden");
-        fnNotice.innerHTML = ``;
-    } 
-}    
-
-const checkLn  = () => {
-    if (lastName.value == ""){
-        lnIcon.classList.remove("hidden");
-        lnNotice.innerHTML = `
-        <p class="notification pink"><em>Last Name cannot be empty</em></p>`
-    } else if (lastName.value != "") {
-        lnIcon.classList.add("hidden");
-        lnNotice.innerHTML = ``;
-    } 
-}
-
-const checkEmail = () => {
-    if (!validateEmail() || email.value =="") {
-        emailIcon.classList.remove("hidden");
-        emailNotice.innerHTML = `
-        <p class="notification pink"><em>Looks like this is not an email</em></p>`
-    } else if (validateEmail) {
-        emailIcon.classList.add("hidden");
-        emailNotice.innerHTML = ``;
+const checkInputs = () => {
+    firstName.value.trim() === "" ? firstName.parentElement.classList.add("error") : firstName.parentElement.classList.remove("error");
+    lastName.value.trim() === "" ? lastName.parentElement.classList.add("error") : lastName.parentElement.classList.remove("error");
+    password.value.trim() === "" ? password.parentElement.classList.add("error") : password.parentElement.classList.remove("error");
+    if (!validateEmail() || email.value.trim() === "") {
+        email.parentElement.classList.add("error")
+    } else if (validateEmail()) {
+        email.parentElement.classList.remove("error")
     }
 }
 
-const checkPw = () => {
-    if (password.value == "") {
-        pwIcon.classList.remove("hidden");
-        pwNotice.innerHTML = `
-        <p class="notification pink"><em>Password cannot be empty</em></p>`
-    } else if (password.value != "") {
-        pwIcon.classList.add("hidden");
-        pwNotice.innerHTML = ``;
-    }
-}
 
 claimBtn.addEventListener('click', checkInputs)
